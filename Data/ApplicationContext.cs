ed92c28eefa94b157fa72067e8492baedb8cf77f
TulioliAles <desmecad@hotmail.com>
2020-10-01 20:50:06 -0300

    Commit 2 - Terminado a implementação das entidades com Fluent API

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using SistemaPedidosEFCore.Domain;
 
 namespace SistemaPedidosEFCore.Data
 {
     class ApplicationContext : DbContext
     {
+        public DbSet<Pedido> Pedidos { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=SistemaPedidosEFCore;Integrated Security=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+
+
         }
     }
 }
---
+++ Data/Configurations/ProdutoConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaPedidosEFCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPedidosEFCore.Data.Configurations
+{
+    class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("Produto");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
+            builder.Property(p => p.Valor).IsRequired();
+            builder.Property(p => p.Descricao).HasColumnType("VARCHAR(60)");
+            builder.Property(p => p.TipoProduto).HasConversion<string>();
+        }
+    }
+}
